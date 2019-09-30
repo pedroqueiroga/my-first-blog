82502412fceea484422468dc6a595eb4ca2d9449
Pedro Queiroga <psq@cin.ufpe.br>
2019-09-29 21:17:44 -0300

    hmmm estranho, antes estava com static root, mas localmente não pegava. agora pega. talvez seja por causa do upgrade de django 2.0 pra 2.2

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIR = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
